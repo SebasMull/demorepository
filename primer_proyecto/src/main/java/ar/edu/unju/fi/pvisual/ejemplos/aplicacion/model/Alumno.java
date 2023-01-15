4d2f59f17bcb046d220306f0a5f4ba1def0ff03d
Sebas <Sebas@DESKTOP-GLF1IUT>
2023-01-15 15:20:41 -0300

    constructor para la clase Alumno

--- primer_proyecto/src/main/java/ar/edu/unju/fi/pvisual/ejemplos/aplicacion/model/Alumno.java
+++ primer_proyecto/src/main/java/ar/edu/unju/fi/pvisual/ejemplos/aplicacion/model/Alumno.java
@@ -4,4 +4,17 @@ public class Alumno {
 	private String nombre;
 	private String apellido;
 	private int edad;
+	
+	public Alumno () {
+		
+	}
+
+	public Alumno(String nombre, String apellido, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+	
+	
 }
